3bb4cc66df45dd6b9b3b4b95e2a3325acf099b03
mnngy <59257197+mnngy@users.noreply.github.com>
2021-05-30 15:07:55 +0900

    회원가입 구현
    php 연동을 통한 회원가입 구현
    
    아이디 중복 예외처리 구현해야 함

--- app/src/main/java/com/example/android_diary/RegisterActivity.java
+++ app/src/main/java/com/example/android_diary/RegisterActivity.java
@@ -1,12 +1,26 @@
 package com.example.android_diary;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText edit_id, edit_pw, edit_pw_confirm;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -15,5 +29,49 @@ public class RegisterActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        edit_id = findViewById(R.id.edit_id);
+        edit_pw = findViewById(R.id.edit_pw);
+        edit_pw_confirm = findViewById(R.id.edit_pw_confirm);
+    }
+
+    public void register(View view) {
+        String userID = edit_id.getText().toString();
+        String userPassword = edit_pw.getText().toString();
+        String userPasswordConfirm = edit_pw_confirm.getText().toString();
+
+        if (userPassword.equals(userPasswordConfirm) == false) {
+            Toast.makeText(getApplicationContext(), "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            Response.Listener<String> responseListener = new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        boolean success = jsonObject.getBoolean("success");
+                        if (success) {
+                            Toast.makeText(getApplicationContext(), "회원 등록에 성공했습니다.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                        }
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            RegisterRequest registerRequest = new RegisterRequest(userID, userPassword, responseListener);
+            System.out.println("1");
+            RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+            System.out.println("2");
+            queue.add(registerRequest);
+            System.out.println("3");
+        }
+    }
+
+    public void cancel(View view) {
+
     }
 }
